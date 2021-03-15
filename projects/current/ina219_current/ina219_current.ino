a4d0f9fcfcd5e3a3d0e0875313a4840f66997d1f
qitas <qitas@qitas.cn>
2021-03-15 14:04:18 +0800

    FIX

--- projects/current/ina219_current/ina219_current.ino
+++ projects/current/ina219_current/ina219_current.ino
@@ -1,4 +1,4 @@
-#include <math.h >
+#include <math.h>
 #include "TM1637.h"
 #include <Adafruit_INA219.h>
 
@@ -21,10 +21,10 @@ float current_mA = 0;
 
 
 void TimeUpdate(void) { int sensorValuesa=current_mA*10;
-	//int sensorValuesa = cbrt(sensorValue) * 100; //取立方根
+	//int sensorValuesa = cbrt(sensorValue) * 100; //取锟斤拷锟斤拷锟斤拷
 	//int sensorValue1 = constrain(sensorValue - 0.5 * 4096 / 2.56, 850, 2500);
 
-//	int sensorValuesa = log10(sensorValue1) * 1000; //以10为底的对数
+//	int sensorValuesa = log10(sensorValue1) * 1000; //锟斤拷10为锟阶的讹拷锟斤拷
 
 //	int voltage = sensorValue * (2.560 / 4096.0) * 1000;
 	//int BRIGHTS = constrain(sensorValuesa, 2800, 3200);
@@ -84,23 +84,23 @@ void setup() {
 	tm1637.set(BRIGHT_TYPICAL); //BRIGHT_TYPICAL = 2,BRIGHT_DARKEST = 0,BRIGHTEST = 7;
 	tm1637.point(POINT_OFF);
 	analogReference(INTERNAL2V56); //use internal 1.1v as Avref
-	//analogReference(EXTERNAL); //使用外部基准
+	//analogReference(EXTERNAL); //使锟斤拷锟解部锟斤拷准
 
 //analogReference(type)
-//描述
-//配置用于模拟输入的基准电压（即输入范围的最大值）。选项有:
-//DEFAULT：默认5V（Ocrobot控制板为5V）或3.3伏特（Ocrobot控制板为3.3V）为基准电压。
-//INTERNAL：在ATmega168和ATmega328上以1.1V为基准电压，以及在ATmega8上以2.56V为基准电压（Ocrobot Mega无此选项）
-////INTERNAL1V1：以1.1V为基准电压（此选项仅针对Ocrobot Mega）
-//INTERNAL2V56：以2.56V为基准电压（此选项仅针对Ocrobot Mega）
-//EXTERNAL：以AREF引脚（0至5V）的电压作为基准电压。
-//参数
-//type：使用哪种参考类型（DEFAULT, INTERNAL, INTERNAL1V1, INTERNAL2V56, INTERNAL4V096 或者 EXTERNAL）。
-//返回
-//无
+//锟斤拷锟斤拷
+//锟斤拷锟斤拷锟斤拷锟斤拷模锟斤拷锟斤拷锟斤拷幕锟阶硷拷锟窖癸拷锟斤拷锟斤拷锟斤拷敕段э拷锟斤拷锟斤拷值锟斤拷锟斤拷选锟斤拷锟斤拷:
+//DEFAULT锟斤拷默锟斤拷5V锟斤拷Ocrobot锟斤拷锟狡帮拷为5V锟斤拷锟斤拷3.3锟斤拷锟截ｏ拷Ocrobot锟斤拷锟狡帮拷为3.3V锟斤拷为锟斤拷准锟斤拷压锟斤拷
+//INTERNAL锟斤拷锟斤拷ATmega168锟斤拷ATmega328锟斤拷锟斤拷1.1V为锟斤拷准锟斤拷压锟斤拷锟皆硷拷锟斤拷ATmega8锟斤拷锟斤拷2.56V为锟斤拷准锟斤拷压锟斤拷Ocrobot Mega锟睫达拷选锟筋）
+////INTERNAL1V1锟斤拷锟斤拷1.1V为锟斤拷准锟斤拷压锟斤拷锟斤拷选锟斤拷锟斤拷锟斤拷Ocrobot Mega锟斤拷
+//INTERNAL2V56锟斤拷锟斤拷2.56V为锟斤拷准锟斤拷压锟斤拷锟斤拷选锟斤拷锟斤拷锟斤拷Ocrobot Mega锟斤拷
+//EXTERNAL锟斤拷锟斤拷AREF锟斤拷锟脚ｏ拷0锟斤拷5V锟斤拷锟侥碉拷压锟斤拷为锟斤拷准锟斤拷压锟斤拷
+//锟斤拷锟斤拷
+//type锟斤拷使锟斤拷锟斤拷锟街参匡拷锟斤拷锟酵ｏ拷DEFAULT, INTERNAL, INTERNAL1V1, INTERNAL2V56, INTERNAL4V096 锟斤拷锟斤拷 EXTERNAL锟斤拷锟斤拷
+//锟斤拷锟斤拷
+//锟斤拷
 
-	uint32_t guid = (GUID3 << 24) | (GUID2 << 16) | (GUID1 << 8) | GUID0; // 给guid赋值唯一ID
-	//Serial.println(guid); // 串口输出唯一ID
+	uint32_t guid = (GUID3 << 24) | (GUID2 << 16) | (GUID1 << 8) | GUID0; // 锟斤拷guid锟斤拷值唯一ID
+	//Serial.println(guid); // 锟斤拷锟斤拷锟斤拷锟轿ㄒ籌D
 	delay(2000);
 }
 
