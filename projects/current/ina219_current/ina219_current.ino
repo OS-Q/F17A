0ec3d2914e624e46690744641bb9aa7213f7ded7
qitas <qitas@qitas.cn>
2021-03-15 14:41:28 +0800

    fix

--- projects/current/ina219_current/ina219_current.ino
+++ projects/current/ina219_current/ina219_current.ino
@@ -21,10 +21,10 @@ float current_mA = 0;
 
 
 void TimeUpdate(void) { int sensorValuesa=current_mA*10;
-	//int sensorValuesa = cbrt(sensorValue) * 100; //ȡ������
+	//int sensorValuesa = cbrt(sensorValue) * 100; //取立方根
 	//int sensorValue1 = constrain(sensorValue - 0.5 * 4096 / 2.56, 850, 2500);
 
-//	int sensorValuesa = log10(sensorValue1) * 1000; //��10Ϊ�׵Ķ���
+//	int sensorValuesa = log10(sensorValue1) * 1000;//以10为底的对数
 
 //	int voltage = sensorValue * (2.560 / 4096.0) * 1000;
 	//int BRIGHTS = constrain(sensorValuesa, 2800, 3200);
@@ -84,22 +84,20 @@ void setup() {
 	tm1637.set(BRIGHT_TYPICAL); //BRIGHT_TYPICAL = 2,BRIGHT_DARKEST = 0,BRIGHTEST = 7;
 	tm1637.point(POINT_OFF);
 	analogReference(INTERNAL2V56); //use internal 1.1v as Avref
-	//analogReference(EXTERNAL); //ʹ���ⲿ��׼
+	//analogReference(EXTERNAL); 	//使用外部基准
 
 //analogReference(type)
-//����
-//��������ģ������Ļ�׼��ѹ�������뷶Χ�����ֵ����ѡ����:
-//DEFAULT��Ĭ��5V��Ocrobot���ư�Ϊ5V����3.3���أ�Ocrobot���ư�Ϊ3.3V��Ϊ��׼��ѹ��
-//INTERNAL����ATmega168��ATmega328����1.1VΪ��׼��ѹ���Լ���ATmega8����2.56VΪ��׼��ѹ��Ocrobot Mega�޴�ѡ�
-////INTERNAL1V1����1.1VΪ��׼��ѹ����ѡ������Ocrobot Mega��
-//INTERNAL2V56����2.56VΪ��׼��ѹ����ѡ������Ocrobot Mega��
-//EXTERNAL����AREF���ţ�0��5V���ĵ�ѹ��Ϊ��׼��ѹ��
-//����
-//type��ʹ�����ֲο����ͣ�DEFAULT, INTERNAL, INTERNAL1V1, INTERNAL2V56, INTERNAL4V096 ���� EXTERNAL����
-//����
-//��
+//描述
+//配置用于模拟输入的基准电压（即输入范围的最大值）。选项有:
+//DEFAULT：默认5V（Ocrobot控制板为5V）或3.3伏特（Ocrobot控制板为3.3V）为基准电压。
+//INTERNAL：在ATmega168和ATmega328上以1.1V为基准电压，以及在ATmega8上以2.56V为基准电压（Ocrobot Mega无此选项）
+////INTERNAL1V1：以1.1V为基准电压（此选项仅针对Ocrobot Mega）
+//INTERNAL2V56：以2.56V为基准电压（此选项仅针对Ocrobot Mega）
+//EXTERNAL：以AREF引脚（0至5V）的电压作为基准电压。
+//参数
+//type：使用哪种参考类型（DEFAULT, INTERNAL, INTERNAL1V1, INTERNAL2V56, INTERNAL4V096 或者 EXTERNAL）。
 
-	uint32_t guid = (GUID3 << 24) | (GUID2 << 16) | (GUID1 << 8) | GUID0; // ��guid��ֵΨһID
+	uint32_t guid = (GUID3 << 24) | (GUID2 << 16) | (GUID1 << 8) | GUID0; // 给guid赋值唯一ID
 	//Serial.println(guid); // �������ΨһID
 	delay(2000);
 }
@@ -113,7 +111,7 @@ void loop() {
 
 	//  shuntvoltage = ina219.getShuntVoltage_mV();
 	//  busvoltage = ina219.getBusVoltage_V();
-	  current_mA = ina219.getCurrent_mA();
+	current_mA = ina219.getCurrent_mA();
 	//  power_mW = ina219.getPower_mW();
 	//  loadvoltage = busvoltage + (shuntvoltage / 1000);
 
@@ -124,10 +122,7 @@ void loop() {
 	  //Serial.print("Power:         "); Serial.print(power_mW); Serial.println(" mW");
 	//  Serial.println("");
 
-	  delay(200);
-
-
-
+	delay(200);
 	// Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
 	//float voltage = sensorValue * (2.56 / 4096.0);
 	// print out the value you read:
